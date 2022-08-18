3de3bda1024173ca576a98d046328f9eefe89a31
atborg92 <91513378+atborg92@users.noreply.github.com>
2022-08-18 16:44:56 -0700

    2021_mlb_stats_project

--- explore1.sql
+++ explore1.sql
@@ -16,10 +16,11 @@ RENAME COLUMN MyUnknownColumn TO Country;
 DELETE FROM 2021_player_stats
 WHERE length(Team) > 3;
 
-#Exploration:
+#EXPLORATION:
+
 # Win/Loss record and win percentage grouped by month, with overall at the bottom:
 SELECT
-	MONTHNAME(Date) AS Month,
+    MONTHNAME(Date) AS Month,
     SUM(CASE WHEN Rslt LIKE 'W%' THEN 1 ELSE 0 END) AS Wins,
     SUM(CASE WHEN Rslt LIKE 'L%' THEN 1 ELSE 0 END) AS Losses,
     ROUND(SUM(CASE WHEN Rslt LIKE 'W%' THEN 1 ELSE 0 END) / CAST(COUNT(game) AS float), 3) AS Win_Pct
@@ -27,9 +28,9 @@ FROM 2021_giants_batting
 GROUP BY 1
 UNION
 SELECT
-	'Total',
+    'Total',
     SUM(CASE WHEN Rslt LIKE 'W%' THEN 1 ELSE 0 END),
-	SUM(CASE WHEN Rslt LIKE 'L%' THEN 1 ELSE 0 END),
+    SUM(CASE WHEN Rslt LIKE 'L%' THEN 1 ELSE 0 END),
     ROUND(SUM(CASE WHEN Rslt LIKE 'W%' THEN 1 ELSE 0 END) / CAST(COUNT(game) AS float), 3)
 FROM 2021_giants_batting;
 
@@ -62,10 +63,11 @@ SELECT
 FROM 2021_giants_batting bat
 JOIN 2021_giants_pitching pitch ON bat.game = pitch.game
 ;
+
 # Giants Wins, Losses and Win Percentage grouped by opponent
 SELECT
-	opp AS Opponent,
-	SUM(CASE WHEN Rslt LIKE 'W%' THEN 1 ELSE 0 END) AS Wins,
+    opp AS Opponent,
+    SUM(CASE WHEN Rslt LIKE 'W%' THEN 1 ELSE 0 END) AS Wins,
     SUM(CASE WHEN Rslt LIKE 'L%' THEN 1 ELSE 0 END) AS Losses,
     ROUND(SUM(CASE WHEN Rslt LIKE 'W%' THEN 1 ELSE 0 END) / CAST(COUNT(game) AS float), 3) AS Win_Pct
 FROM 2021_giants_batting
@@ -73,22 +75,25 @@ GROUP BY 1
 ORDER BY 4 DESC;
 
 # Each team's top three players in terms of OPS (On base percentage + slugging percentage), sorted by the average among those top three.
-
+# First, I will use a CTE to rank each player by OPS, partitioned by the team they played for in 2021.
 WITH ops_rankings AS (SELECT
-	Team,
-	Player,
+    Team,
+    Player,
     OPS,
     Age,
     ROW_NUMBER() OVER(PARTITION BY Team ORDER BY OPS DESC) AS ops_rank
 FROM 2021_player_stats
 )
+
+# In the outer query, I pull each team's top OPS player, then use a series of subqueries to pull up the 2nd and 3rd place players' names and OPS, so that
+# it appears in one row.
 SELECT
-	ops.Team,
+    ops.Team,
     ops.Player AS first_name,
     ops.OPS AS first_ops,
-	(SELECT Player FROM ops_rankings ops2 WHERE ops_rank = 2 AND ops.Team = ops2.Team) AS second_name,
+    (SELECT Player FROM ops_rankings ops2 WHERE ops_rank = 2 AND ops.Team = ops2.Team) AS second_name,
     (SELECT OPS FROM ops_rankings ops2 WHERE ops_rank = 2 AND ops.Team = ops2.team) AS second_ops,
-	(SELECT Player FROM ops_rankings ops3 WHERE ops_rank = 3 AND ops.Team = ops3.Team) AS third_name,
+    (SELECT Player FROM ops_rankings ops3 WHERE ops_rank = 3 AND ops.Team = ops3.Team) AS third_name,
     (SELECT OPS FROM ops_rankings ops3 WHERE ops_rank = 3 AND ops.Team = ops3.team) AS third_ops,
     ROUND((ops.OPS +  (SELECT OPS FROM ops_rankings ops2 WHERE ops_rank = 2 AND ops.Team = ops2.team) +  
     (SELECT OPS FROM ops_rankings ops3 WHERE ops_rank = 3 AND ops.Team = ops3.team))/3,3) AS top_three_avg
@@ -97,31 +102,33 @@ GROUP BY 1
 ORDER BY 8 DESC
 ;
 
+# I want to see how ballpark dimensions and wall height might affect home team offensive performance. 
+# To do this I will create a new view that includes each ballpark's dimensions and wall height and their ranking among other ballparks.
+
 #Create a view of each stadium with it's ranking of relative size (avg depth)
-#DROP VIEW IF EXISTS dimension_rankings
 
 DROP VIEW IF EXISTS dimension_rankings;
 
 CREATE VIEW bbref.dimension_rankings AS 
 WITH dimensions AS (SELECT
-	*,
+    *,
     ROUND((lf_dim + cf_dim + rf_dim) / 3,3) AS avg_depth,
-	RANK() OVER(order by ROUND((lf_dim + cf_dim + rf_dim) / 3,3) DESC) AS depth_rank,
+    RANK() OVER(order by ROUND((lf_dim + cf_dim + rf_dim) / 3,3) DESC) AS depth_rank,
     ROUND((lf_w + cf_w + rf_w) / 3,3) AS avg_wall_height,
     RANK() OVER(ORDER BY ROUND((lf_w + cf_w + rf_w) / 3,3) DESC) AS height_rank
 FROM park_dimensions
 ORDER BY 12,14)
 
 SELECT
-	*,
+    *,
     CASE WHEN depth_rank <= 10 THEN "Large" WHEN depth_rank BETWEEN 11 AND 20 THEN "Medium" ELSE "Small" END AS depth,
     CASE WHEN height_rank <= 10 THEN "High" WHEN height_rank BETWEEN 11 AND 20 THEN "Medium" ELSE "Short" END AS wall_height
 FROM dimensions;
 
-#Each ballpark, its depth and height rankings, and sum of homeruns hit by the hometeam in 2021
+#Each ballpark's depth and height rankings, and sum of homeruns hit by the hometeam in 2021:
 
 SELECT
-	name AS ballpark,
+    name AS ballpark,
     depth_rank,
     height_rank,
     SUM(splits.hr) AS hometeam_hrs
@@ -131,10 +138,10 @@ WHERE splits.split = 'Home'
 GROUP BY 1,2,3;
 
 
-#Using dimension rankings view and home away batting splits, I looked at average home runs per season hit by the home team grouped by wall height and depth. Used a rollup function to examine the differences when accounting 
-#for both wall size and depth, as well as each individually.
+#Using dimension rankings view and home away batting splits, I looked at average home runs per season hit by the home team grouped by wall height and depth. 
+#Used a rollup function to examine the differences when accounting for both wall size and depth, as well as each individually.
 SELECT 
-	IF(GROUPING(depth), 'All depths', depth) AS depth,
+    IF(GROUPING(depth), 'All depths', depth) AS depth,
     IF(GROUPING(wall_height), 'All heights',wall_height) AS wall_height,
     AVG(splits.hr) AS avg_season_hr
 FROM dimension_rankings parks
@@ -143,10 +150,10 @@ WHERE split = 'Home'
 GROUP BY depth, wall_height WITH ROLLUP
 ORDER BY 3 DESC;
 
-#Cleaned up version with fewer groupings
+#The rollup output was a bit messy, so here is a cleaned up version with fewer groupings:
 
 SELECT 
-	depth,
+    depth,
     'All heights' AS wall_height,
     AVG(splits.hr) AS avg_season_hr
 FROM dimension_rankings parks
@@ -155,7 +162,7 @@ WHERE split = 'Home'
 GROUP BY 1
 UNION ALL
 SELECT 
-	'All depths',
+    'All depths',
     wall_height,
     AVG(splits.hr) AS avg_season_hr
 FROM dimension_rankings parks
@@ -164,7 +171,7 @@ WHERE split = 'Home'
 GROUP BY 2
 UNION ALL
 SELECT
-	'All depths',
+    'All depths',
     'All heights',
     AVG(splits.hr)
 FROM dimension_rankings parks
@@ -175,17 +182,18 @@ order by field(wall_height,'short','medium','high'),field(depth,'Small','Medium'
 
 #Giants with running total of homeruns
 SELECT
-	EXTRACT(month FROM date) AS month,
+    EXTRACT(month FROM date) AS month,
     date,
     SUM(hr) OVER(ORDER BY date ROWS UNBOUNDED PRECEDING) AS hr_running_total
 FROM 2021_giants_batting
 ;
-#Creating view of Giants batting by month
+
+# To dive deeper into Giants monthly stats, I will create a view of Giants batting by month
 
 DROP VIEW IF EXISTS bbref.giants_batting_monthly;
 CREATE VIEW bbref.giants_batting_monthly AS
 SELECT
-	MONTHNAME(bat.date) AS Month,
+    MONTHNAME(bat.date) AS Month,
     EXTRACT(MONTH FROM bat.date) AS monthnum,
     SUM(bat.pa) as PA,
     SUM(bat.ab) as ab,
@@ -218,9 +226,9 @@ GROUP BY 1;
 
 #Using above view, here is running total select stats by month in 2021
 SELECT
-	month,
+    month,
     ROUND(SUM(h) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING) / SUM(ab) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING),3) AS ba_runningtotal,
-	SUM(pa) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING) AS pa_runningtotal,
+    SUM(pa) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING) AS pa_runningtotal,
     SUM(r) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING) AS r_runningtotal,
     SUM(hr) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING) AS hr_runningtotal,
     ROUND(SUM(pitching_so) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING) / SUM(bb_allowed) OVER(ORDER BY monthnum ROWS UNBOUNDED PRECEDING),3) AS k_bb_runningtotal
@@ -232,15 +240,14 @@ FROM giants_batting_monthly
 
 
 WITH salary_rank AS (SELECT
-	Team,
+    Team,
     Attendance,
     salary,
     NTILE(3) OVER(ORDER BY salary DESC) AS third
 FROM 2021_misc_team_stats
-
 )
 SELECT
-	Team,
+    Team,
     Attendance,
     ROUND((attendance - (SELECT AVG(attendance) FROM 2021_misc_team_stats)) / 
     (SELECT STD(attendance) FROM 2021_misc_team_stats),3) AS attendance_zscore,
@@ -258,7 +265,7 @@ ORDER BY attendance DESC
 #Over .500 Teams that overpreformed pythagorean record AND whose salary was bottom third of the league
 
 WITH pyth AS (SELECT
-	bat.tm,
+    bat.tm,
     bat.win_pct,
     ROUND(POWER(bat.r,2) / (POWER(bat.r,2) + POWER(pit.r,2)),3) AS Pythagorean,
     round((bat.win_pct / ROUND(POWER(bat.r,2) / (POWER(bat.r,2) + POWER(pit.r,2)),3)-1)*100,3) AS dif,
@@ -270,7 +277,7 @@ JOIN 2021_team_pitching pit ON bat.tm = pit.tm
 JOIN 2021_misc_team_stats misc ON bat.tm = misc.team)
 
 SELECT
-	pyth.tm,
+    pyth.tm,
     pyth.win_pct,
     CONCAT("$",salary) AS salary,
     ROUND((salary - (SELECT AVG(salary) FROM 2021_misc_team_stats)) / (SELECT STD(salary) FROM 2021_misc_team_stats),3) AS salary_zscore,
@@ -282,9 +289,3 @@ WHERE salary_tier = 3
 AND win_pct >= .5
 ORDER BY 3 DESC;
 
-
-
-#Tableau: Map player home countries / states and stats
-
-#Python ideas: matplotlib attendance & win pct
-#Seaborn pairplot (sic?)
